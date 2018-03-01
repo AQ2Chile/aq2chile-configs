a514082f14a433cda223e0e9bdc511e2a3df96ff
Feña Agar <fernando.agar@gmail.com>
2018-03-01 12:30:37 +0100

    Cleaned up

--- files/action/h_admin/make_maplist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ACTIONDIR=./..
-MAPDIR=$ACTIONDIR/maps
-CFGDIR=$ACTIONDIR/config
-cd $MAPDIR
-ls -1 *bsp | sed s/.bsp// > $CFGDIR/maplist.ini.current
-cd $CFGDIR
-sort maplist.ini.current > maplist.ini
